f432a6165b69f23a97e3f0b6fde8078cbac6a4d7
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2019-01-17 00:58:08 +0300

    loops

---
+++ ts11_Loops/loops.ts
@@ -0,0 +1,9 @@
+let sehirler: string[] = ["Ankara", "İstanbul", "İzmir"];
+
+// for (let sehir in sehirler) {
+//     console.log(sehir);
+// }
+
+for (let sehir of sehirler) {
+    console.log(sehir);
+}
