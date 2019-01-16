221b669565a643208e09f6866167dd94ded5adc2
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2019-01-16 22:38:04 +0300

    examples

---
+++ ts01_intro/intro.js
@@ -0,0 +1,24 @@
+function selamVer(isim) {
+    return "Merhaba " + isim;
+}
+var mesaj = selamVer("Veli KOŞAR");
+console.log(mesaj);
+var sayi;
+sayi = 10;
+sayi = 10.4;
+// sayi = "Ankara";
+var sehir = "Adana";
+sehir = "Ankara";
+sehir = "İstanbul";
+// sehir = 10;
+var dogruMu = true;
+dogruMu = true;
+dogruMu = false;
+// dogruMu = 1;
+// let sayilar = [1, 2, 3]; // javascript
+var sayilar = [1, 2, 3, 4];
+// let sayilar: number[] = [1, 2, 3, 4, "Ankara"];
+var sayilar2 = [1, 2, 3];
+var dizi = [2, "Ankara"];
+console.log(typeof dizi[0]);
+console.log(typeof dizi[1]);
--- ts01_intro/intro.ts
+++ ts01_intro/intro.ts
@@ -2,6 +2,62 @@ function selamVer(isim: string): string {
     return "Merhaba " + isim;
 }
 
-let mesaj: string = selamVer("ali");
+let mesaj: string = selamVer("Veli KOŞAR");
 
 console.log(mesaj);
+
+let sayi: number;
+
+sayi = 10;
+
+sayi = 10.4;
+// sayi = "Ankara";
+
+let sehir: string = "Adana";
+sehir = "Ankara";
+sehir = "İstanbul";
+// sehir = 10;
+
+let dogruMu: boolean = true;
+dogruMu = true;
+dogruMu = false;
+// dogruMu = 1;
+
+// let sayilar = [1, 2, 3]; // javascript
+let sayilar: number[] = [1, 2, 3, 4];
+// let sayilar: number[] = [1, 2, 3, 4, "Ankara"];
+
+let sayilar2: Array<number> = [1, 2, 3];
+
+let dizi: [number, string] = [2, "Ankara"]; // tuple
+console.log(typeof dizi[0]);
+console.log(typeof dizi[1]);
+
+enum Renk { Kırmızı = 1, Siyah, Mavi }
+
+let renk: Renk = Renk.Kırmızı;
+
+console.log(renk);
+
+let deger: any = "Ankara";
+deger = 2;
+deger = {};
+
+let deger2: void = undefined;
+
+function selamVer2(): void {
+    console.log("Merhaba");
+}
+
+// undefined null
+
+let yas: number;
+yas = 10;
+
+
+class Musteri{
+}
+
+let musteri: Musteri = null;
+
+console.log(musteri);
